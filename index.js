fdfc74bb16e18f177c50a4e9af152d42bb4d0132
Quince Lewis <106302729+quinceosanya@users.noreply.github.com>
2022-06-21 11:09:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution here!
+// const divide= (function(number1,number2){
+// const division = number1 / number2;
+//     return division;
+// })
+
+// divide(200,10);
+
+//Arrow function 
+// let divide = function divideExpression(number1,number2){
+//     let divison = number1/number2;
+//     return divison;
+// }
+
+// console.log(divide(200,10));
+
+const divide = function(){
+    return 200/10;
+}
+
+const square = number => number * number;
+
+const add = (paramOne,paramTwo) => paramOne + paramTwo;
